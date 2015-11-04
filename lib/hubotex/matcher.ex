366af63f032fa54c222a4c2408f9cd2649abc809
Avinasha Shastry <me@avinasha.com>
2015-11-04 18:33:06 +0530

    Refactors code to extract the matcher out of the robot

---
+++ lib/hubotex/matcher.ex
@@ -0,0 +1,28 @@
+defmodule Hubotex.Matcher do
+  def match(message, rules) do
+    {message, rules}
+    |> map_rules
+    |> reject_no_match
+    |> acc_results
+  end
+
+  defp map_rules({message, rules}) do
+    results = Enum.map(rules, fn rule ->
+      {regex, consequence} = rule
+      if rule_match?(regex, message), do: {:ok, consequence.(message)}, else: {:nomatch}
+    end)
+    {message, results}
+  end
+ 
+  defp reject_no_match({message, results}) do
+    results = Enum.reject(results, fn result -> result == {:nomatch} end)
+    {message, results}
+  end
+  
+  defp acc_results({message, []}), do: {:nomatch, message}
+  defp acc_results({_message, results}), do: Enum.reduce(results, fn {:ok, result}, {:ok, response} -> {:ok, "#{response}\n#{result}"} end)
+  
+  defp rule_match?(regex, message) do
+    Regex.match?(regex, message)
+  end 
+end
--- lib/hubotex/robot.ex
+++ lib/hubotex/robot.ex
@@ -15,9 +15,6 @@ defmodule Hubotex.Robot do
     {:reply, do_accept(message, state), state}
   end
 
-
-
-  
   defp do_accept(message, state) do
     {message, state}
     |> do_match
@@ -25,34 +22,8 @@ defmodule Hubotex.Robot do
   end
 
   defp do_match({message, state}) do
-    {message, state[:rules]}
-    |> map_rules
-    |> reject_no_match
-    |> acc_results
+    Hubotex.Matcher.match(message, state[:rules])
   end
 
   defp do_respond(message), do: message
-
-
-  defp map_rules({message, rules}) do
-    results = Enum.map(rules, fn rule ->
-      {regex, consequence} = rule
-      if rule_match?(regex, message), do: {:ok, consequence.(message)}, else: {:nomatch}
-    end)
-    {message, results}
-  end
-
-  defp reject_no_match({message, results}) do
-    results = Enum.reject(results, fn result -> result == {:nomatch} end)
-    {message, results}
-  end
-
-  defp acc_results({message, []}), do: {:nomatch, message}
-  defp acc_results({_message, results}), do: Enum.reduce(results, fn {:ok, result}, {:ok, response} -> {:ok, "#{response}\n#{result}"} end)
-
-
-
-  defp rule_match?(regex, message) do
-    Regex.match?(regex, message)
-  end
 end
